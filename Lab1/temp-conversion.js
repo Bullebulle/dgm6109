27f942b822c56d96e5fb8e066b2aba87b4ff4e0e
Bullebulle <1102559942@qq.com>
2025-09-23 03:17:34 -0700

    Week #1 Lab Homework

---
+++ Lab1/temp-conversion.js
@@ -0,0 +1,6 @@
+let fahrenheit = 12.2 // Qiqihar 1991
+let celsius = (fahrenheit -32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log("Temperature (celsius): " + celsius)
+console.log("Temperature (kelvin): " + kelvin)
